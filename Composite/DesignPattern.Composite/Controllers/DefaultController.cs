e0f31bd0152cca220d6dbdf73b6cbf8e4b14a732
ArdaCimen <cimenarda.yaz@gmail.com>
2024-09-05 13:31:36 +0300

    Composite Design Pattern

---
+++ Composite/DesignPattern.Composite/Controllers/DefaultController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DesignPattern.Composite.DAL;
+using DesignPattern.Composite.CompositePattern;
+using Microsoft.EntityFrameworkCore;
+namespace DesignPattern.Composite.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly Context _context;
+
+        public DefaultController(Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+
+            var categories = _context.Categories.Include(x => x.Products).ToList();
+
+            var values = Recursive(categories, new Category { CategoryName = "First Category", CategoryID = 0 }, new ProductComposite(0, "First Composite"));
+
+            ViewBag.v = values;
+
+
+            return View();
+        }
+
+        public ProductComposite Recursive(List<Category> categories,Category firstCategory,ProductComposite FirstComposite,ProductComposite leaf=null)
+        {
+            categories.Where(x => x.UpperCategoryID == firstCategory.CategoryID).ToList().ForEach(y =>
+            {
+                var productComposite = new ProductComposite(y.CategoryID, y.CategoryName);
+
+                y.Products.ToList().ForEach(z =>
+                {
+                    productComposite.Add(new ProductComponent(z.ProductID, z.ProductName));
+                });
+                if (leaf!=null)
+                {
+                    leaf.Add(productComposite);
+
+                }
+                else
+                {
+                    FirstComposite.Add(productComposite);
+                }
+
+                Recursive(categories, y, FirstComposite, productComposite);
+            });
+
+            return FirstComposite;
+
+        }
+    }
+}
---
+++ Composite/DesignPattern.Composite/DAL/Context.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesignPattern.Composite.DAL
+{
+    public class Context :DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=ARDACIMEN\\SQLEXPRESS;initial catalog=CompositeDesignPattern;integrated security=true;");
+
+
+        }
+
+
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
