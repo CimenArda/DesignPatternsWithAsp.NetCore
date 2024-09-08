6d6b7fa0dad78a6e12b1d4ed6bab0eb6669cea83
ArdaCimen <cimenarda.yaz@gmail.com>
2024-09-08 13:49:37 +0300

    Iterator Design Pattern

---
+++ Iterator/DesignPattern.Iterator/Iterator/IIterator.cs
@@ -0,0 +1,20 @@
+﻿namespace DesignPattern.Iterator.Iterator
+{
+    public interface IIterator<T>
+    {
+        T CurrentItem { get; }   //Aktif Öge
+
+        bool NextLocation();  //Başka Öge var Mı?
+
+
+
+
+
+     
+
+
+
+
+
+    }
+}
---
+++ Iterator/DesignPattern.Iterator/Iterator/VisitRouteIterator.cs
@@ -0,0 +1,24 @@
+﻿namespace DesignPattern.Iterator.Iterator
+{
+    public class VisitRouteIterator : IIterator<VisitRoute>
+    {
+        public VisitRouteIterator(VisitRouteMover visitRouteMover)
+        {
+            _visitRouteMover = visitRouteMover;
+        }
+        private int CurrentIndex = 0;
+        private  VisitRouteMover _visitRouteMover { get; set; }
+
+        public VisitRoute CurrentItem { get; set; }
+
+        public bool NextLocation()
+        {
+           if (CurrentIndex <_visitRouteMover.VisitRouteCount)
+            {
+                CurrentItem = _visitRouteMover.visitRoutes[CurrentIndex++];
+                return true;
+            }
+           return false;
+        }
+    }
+}
